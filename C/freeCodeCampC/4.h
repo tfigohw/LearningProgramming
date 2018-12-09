aeaa7c71e595e87d192f73587c0f2f806485940b
tfigohw <45695868+tfigohw@users.noreply.github.com>
2018-12-09 18:47:25 +0100

    Update 4.h

--- C/freeCodeCampC/4.h
+++ C/freeCodeCampC/4.h
@@ -10,10 +10,10 @@ void workingWithNumbers()
 	printf("%f\n", 8.9);
 	printf("%f\n", 5 / 3.0);
 	printf("%d\n", num % 3);
-	printf("%f\n", pow(2,3)); 	  /* power function from math.h*/
-	printf("%f\n", sqrt(17));     /* same */
-	printf("%f\n", ceil(17.1));   /* same */
-	printf("%f\n", floor(16.91)); /* same */
+	printf("%f\n", pow(2,3)); 	/* power function from math.h*/
+	printf("%f\n", sqrt(17));       /* same */
+	printf("%f\n", ceil(17.1));     /* same */
+	printf("%f\n", floor(16.91));   /* same */
 }
 
 void workingWithConstants()
